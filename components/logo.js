792b4f2843ac6626a61ac18fbbbf5d96f6d1ffcf
Triloworld <2671726+Triloworld@users.noreply.github.com>
2021-10-26 21:36:03 +0200

    Logo fix. Package update

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const logo = () => (
-    <img src="/static/logo.svg" alt="Skład Olesno" width="83px" height="30px" style="max-height: 30px;height: 100%;width: 100%;margin: auto;display: block;"/>
+    <img src="/static/logo.svg" alt="Skład Olesno" width="83px" height="30px" style={{'display': 'block'}} />
   );
 
 export default logo;
